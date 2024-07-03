a99ec31542d7288165c9e0641580545622ddcea0
PaulRusz <paulruszkay@gmail.com>
2024-07-03 17:15:13 -0400

    added login code to fix error, fixed post.jsx to display post in containers on post page with css styling

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -14,7 +14,7 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
     mutation addUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!,$avatar:String!) {
-        addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password,avatar: $avatar) {
+        addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password, avatar: $avatar) {
             token
             user {
                 _id
@@ -23,3 +23,22 @@ export const ADD_USER = gql`
         }
     }
 `;
+
+
+export const ADD_POST = gql`
+mutation addPost($postTitle: String!, $postText: String!, $postTag: String!, $username: String!) {
+    addPost(postTitle: $postTitle, postText: $postText, postTag: $postTag, username: $username) {
+        _id
+        postTitle
+        postText
+        postTag
+        username
+        createdAt
+        comments {
+            _id
+            commentText
+            createdAt
+            username
+            }
+    }
+}`
